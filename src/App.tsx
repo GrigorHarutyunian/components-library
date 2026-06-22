9beb94f1478056ebe5146a57c55c5374fc0677bd
Grigor Harutyunyan <grigorharutyunian97@gmail.com>
2026-06-22 23:13:33 +0400

    add Button component in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,64 @@
 import "./App.css";
+import Button from "./components/Button/Button";
 
 function App() {
-  return <div>1</div>;
+  return (
+    <>
+      <Button
+        size="small"
+        color="success"
+        variant="text"
+        href=""
+        onClick={() => console.log("a")}
+      >
+        Small
+      </Button>
+      <Button
+        size="medium"
+        color="secondary"
+        variant="contained"
+        href=""
+        onClick={() => console.log("b")}
+      >
+        Medium
+      </Button>
+      <Button
+        size="large"
+        color="primary"
+        variant="outlined"
+        href=""
+        onClick={() => console.log("c")}
+      >
+        Large
+      </Button>
+
+      <Button
+        size="small"
+        color="success"
+        variant="text"
+        href="https://www.youtube.com/"
+      >
+        Small
+      </Button>
+      <Button
+        size="medium"
+        color="secondary"
+        variant="contained"
+        href="https://www.youtube.com/"
+      >
+        Medium
+      </Button>
+      <Button
+        size="large"
+        color="primary"
+        variant="outlined"
+        href="https://www.youtube.com/"
+        onClick={() => console.log("f")}
+      >
+        Large
+      </Button>
+    </>
+  );
 }
 
 export default App;
