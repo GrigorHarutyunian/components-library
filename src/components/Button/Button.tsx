1ca37c66eb5d8dba0db09fbbee87b8e303f76ccd
Grigor Harutyunyan <grigorharutyunian97@gmail.com>
2026-06-22 23:12:46 +0400

    add button component and style for button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,45 @@
+import clsx from "clsx";
+import type { Size, Color, Variant } from "../../models";
+import "./Button.css";
+import type { ReactNode } from "react";
+interface IButtonProps {
+  size: Size;
+  color: Color;
+  variant: Variant;
+  href: string;
+  disabled?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>;
+  children: ReactNode;
+}
+
+const Button = ({
+  size,
+  color,
+  children,
+  variant,
+  href,
+  onClick,
+  disabled,
+}: IButtonProps) => {
+  const className = clsx(
+    "button",
+    `button--${size}`,
+    `button--${color}`,
+    `button--${variant}`,
+    {
+      "is-disabled": disabled,
+    },
+  );
+
+  return href ? (
+    <a className={className} href={href} onClick={onClick}>
+      {children}
+    </a>
+  ) : (
+    <button className={className} disabled={disabled} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
