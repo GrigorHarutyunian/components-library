5ed2fd2f8dfcfa8d9e19531c09db5db5dea4c469
Grigor Harutyunyan <grigorharutyunian97@gmail.com>
2026-06-24 22:45:30 +0400

    add modal in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 import Button from "./components/Button/Button";
+import Modal from "./components/Modal/Modal";
 
 function App() {
+  const [open, setOpen] = useState<boolean>(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <>
+      <Modal open={open} onClose={handleClose}>
+        <h1>Modal Header</h1>
+        <p>Modal Content</p>
+      </Modal>
       <Button
         size="small"
         color="success"
@@ -18,7 +27,7 @@ function App() {
         color="secondary"
         variant="contained"
         href=""
-        onClick={() => console.log("b")}
+        onClick={handleOpen}
       >
         Medium
       </Button>
