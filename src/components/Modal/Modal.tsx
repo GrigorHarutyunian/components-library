e92d6f56131a93249ef1e995c7b22f0659a05e26
Grigor Harutyunyan <grigorharutyunian97@gmail.com>
2026-06-25 17:41:26 +0400

    add backdrop in modal,non scrol in modal time

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,20 +1,33 @@
 import { createPortal } from "react-dom";
 import "./Modal.css";
+import { useEffect } from "react";
+import clsx from "clsx";
 
 interface IModalProps {
   open: boolean;
   onClose: () => void;
   children: React.ReactNode;
+  className?: string;
 }
 
-const Modal = ({ open, onClose, children }: IModalProps) => {
+const Modal = ({ open, onClose, children, className }: IModalProps) => {
+  useEffect(() => {
+    document.documentElement.style.overflow = open ? "hidden" : "";
+
+    return () => {
+      document.documentElement.style.overflow = "";
+    };
+  }, [open]);
+
   const modal = (
-    <div onClick={onClose} className="cl-modal">
-      <div
-        className="cl-modal__content"
-        onClick={(e: React.MouseEvent<HTMLDivElement>) => e.stopPropagation()}
-      >
-        {children}
+    <div onClick={onClose} className={clsx(className, "cl-modal")}>
+      <div className={clsx("cl-modal__backdrop")}>
+        <div
+          className="cl-modal__content"
+          onClick={(e: React.MouseEvent<HTMLDivElement>) => e.stopPropagation()}
+        >
+          {children}
+        </div>
       </div>
     </div>
   );
